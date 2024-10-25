b04589a0bc149739418d1765aa804d15d0ab20b1
Danny Cárdenas <danny.cardenas@bayteq.com>
2024-10-25 16:50:13 -0500

    Cap 62

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterModule } from "./counter/counter.module";
-import { HeoresModule } from './heroes/heroes.module';
 import { DbzModule } from './dbz/dbz.module';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +14,6 @@ import { DbzModule } from './dbz/dbz.module';
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    HeoresModule,
     DbzModule
   ],
   providers: [],
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -6,7 +6,7 @@ import { CommonModule } from "@angular/common";
 @NgModule({
     declarations: [
         HeroComponent,
-        ListComponent
+        ListComponent,
     ],
     exports: [
         HeroComponent,
